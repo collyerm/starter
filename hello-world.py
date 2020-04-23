8df18daf9e1ada2104dc9899f4d3de54b69205ac
collyerm <matt_887@hotmail.com>
2020-04-23 13:22:20 +1000

    Added Flask functionality

--- hello-world.py
+++ hello-world.py
@@ -1,2 +1,12 @@
+from flask import Flask
 
-print('Helloo, world!')
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
